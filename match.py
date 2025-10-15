f21cf5ca78b7cae4f275f5934f397943836399c4
Tican <dlwogns0813@gmail.com>
2025-10-15 13:04:19 +0000

    learning match-case

---
+++ match.py
@@ -0,0 +1,13 @@
+#Match-case 문 사용 (if, elif 대신 사용)
+
+
+
+name = input("What's your name?")
+
+match name :
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print"Who?
---
+++ pythonic.py
@@ -0,0 +1,11 @@
+# Pythonic 코드란?
+# 다른 언어보다 간결하고 읽기 쉬운 코드를 의미함!
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+# 더 간결하게는
+def is_even(n):
+    return if n % 2 == 0
+
+
