c2b2770268fe5a4af9878cd2c63319441ae07963
Tican <dlwogns0813@gmail.com>
2025-10-18 06:54:38 +0000

    learning python

--- mario.py
+++ mario.py
@@ -1,1 +1,44 @@
-git add 
+# mario 예제
+def main():
+    print_column(3)
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+    
+main()
+
+# 가로줄 출력
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("?" * width)
+
+main()
+
+# 정사각형(square) 출력
+def main():
+    print_square(3)
+
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+main()
+
+
+# 더 깔끔한 버전
+def main():
+    print_square(3)
+
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+    print("#" * width)
+
+main()
