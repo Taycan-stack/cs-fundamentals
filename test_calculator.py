6bc7e43d13a38ffa27717185e34881098f2e2e31
Tican <dlwogns0813@gmail.com>
2025-10-23 11:26:19 +0000

    learning unit test

--- test_calculator.py
+++ test_calculator.py
@@ -2,5 +2,7 @@ from calculator import square
 
 def test_square():
     assert square(2) == 4
-    assert square(3) == 10
+    assert square(3) == 30
 
+if __name__ == "__main__": # 이 파일이 메인 요리일 때만 아래 코드를 돌려라
+    test_square()
