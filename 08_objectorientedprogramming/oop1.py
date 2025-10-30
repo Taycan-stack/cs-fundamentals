57eb2a5428bfd9ae220ced94e52c36ac5d88d024
Tican <dlwogns0813@gmail.com>
2025-10-30 12:24:40 +0000

    pratice oop

--- 08_objectorientedprogramming/oop1.py
+++ 08_objectorientedprogramming/oop1.py
@@ -34,4 +34,50 @@ def get_student():
 if __name__ == "__main__":
     main()
 
-    
+# 리스트로 바꾸기
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    name = input("Name: ")    
+    house = input("House: ")
+    return [name, house]
+
+if __name__ == "__main__":
+    main()
+
+# 딕셔너리 사용하기
+def main():
+    student = get_student()
+    print(f"{student['name'] from {student['house']}}")    
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name" : name, "house": house}
+
+if __name__=="__main__":
+    main()
+
+
+
+# 객체로 표현하기
+class Student:
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")        
+
+def get_student():
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+if __name__ == "__main__"    :
+    main()
+
+# 객체 지향 프로그래밍은 ‘데이터(정보)’와 ‘행동(함수)’을 하나의 객체로 묶는 방식이에요.
